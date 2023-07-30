e2e517ce574bd7c270f0cd083f1c22b71c9fa898
PabloDinella <pablo.dinella@gmail.com>
2023-07-30 17:09:17 -0300

    add posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Suspense } from "react";
+import { PostHogPageview, PHProvider } from "./providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Palavras de Conforto',
-  description: 'Versículos com uma mensagem de alento para você ou um amigo!',
-}
+  title: "Palavras de Conforto",
+  description: "Versículos com uma mensagem de alento para você ou um amigo!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body className={inter.className}>{children}</body>
+      </PHProvider>
     </html>
-  )
+  );
 }
